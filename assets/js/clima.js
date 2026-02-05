ffa35da97147558c25dd51db4359149863c40267
Javier VH <javiervargas9519gmail.com>
2026-02-05 19:39:49 -0300

    Actualizado con información del clima, tamaño de cards y estilos en css

---
+++ assets/js/clima.js
@@ -0,0 +1,13 @@
+const climaCiudades = [
+  { ciudad: "Antofagasta", temperatura: 26, estado: "Soleado" },
+  { ciudad: "Calama", temperatura: 24, estado: "Despejado" },
+  { ciudad: "Tocopilla", temperatura: 22, estado: "Nublado" },
+  { ciudad: "Mejillones", temperatura: 23, estado: "Viento" },
+  { ciudad: "Taltal", temperatura: 21, estado: "Nublado" },
+  { ciudad: "San Pedro de Atacama", temperatura: 28, estado: "Seco" },
+  { ciudad: "María Elena", temperatura: 25, estado: "Soleado" },
+  { ciudad: "Sierra Gorda", temperatura: 20, estado: "Viento" },
+  { ciudad: "Ollagüe", temperatura: 15, estado: "Frío" },
+  { ciudad: "Balneario Hornitos", temperatura: 19, estado: "Nublado" },
+  { ciudad: "Juan López", temperatura: 27, estado: "Soleado" }
+];
