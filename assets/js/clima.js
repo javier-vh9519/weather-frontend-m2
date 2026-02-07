295fa8764dd357c9e118e547fce3dc66d04b609a
Javier VH <javiervargas9519gmail.com>
2026-02-07 18:19:17 -0300

    Agregada a la rama toda la informacion sobre el clima más un mapa y widget del pronostico

--- assets/js/clima.js
+++ assets/js/clima.js
@@ -11,3 +11,140 @@ const climaCiudades = [
   { ciudad: "Balneario Hornitos", temperatura: 19, estado: "Nublado" },
   { ciudad: "Juan López", temperatura: 27, estado: "Soleado" }
 ];
+
+
+const contenidoPronostico = [
+{
+  ciudad: "Antofagasta",
+  temperatura: 26,
+  estado: "Soleado",
+  semanal: [
+    { dia: "Lun", temp: 25, estado: "☀️" },
+    { dia: "Mar", temp: 24, estado: "☀️" },
+    { dia: "Mié", temp: 22, estado: "⛅" },
+    { dia: "Jue", temp: 21, estado: "⛅" },
+    { dia: "Vie", temp: 23, estado: "☀️" }
+  ]
+},
+{
+  ciudad: "Calama",
+  temperatura: 26,
+  estado: "Soleado",
+  semanal: [
+    { dia: "Lun", temp: 25, estado: "☀️" },
+    { dia: "Mar", temp: 24, estado: "☀️" },
+    { dia: "Mié", temp: 22, estado: "⛅" },
+    { dia: "Jue", temp: 21, estado: "⛅" },
+    { dia: "Vie", temp: 23, estado: "☀️" }
+  ]
+},
+{
+  ciudad: "Tocopilla",
+  temperatura: 22,
+  estado: "Nublado",
+  semanal: [
+    { dia: "Lun", temp: 25, estado: "☀️" },
+    { dia: "Mar", temp: 24, estado: "☀️" },
+    { dia: "Mié", temp: 22, estado: "⛅" },
+    { dia: "Jue", temp: 21, estado: "⛅" },
+    { dia: "Vie", temp: 23, estado: "☀️" }
+  ]
+},
+{
+  ciudad: "Mejillones",
+  temperatura: 23,
+  estado: "Viento",
+  semanal: [
+    { dia: "Lun", temp: 25, estado: "☀️" },
+    { dia: "Mar", temp: 24, estado: "☀️" },
+    { dia: "Mié", temp: 22, estado: "⛅" },
+    { dia: "Jue", temp: 21, estado: "⛅" },
+    { dia: "Vie", temp: 23, estado: "☀️" }
+  ]
+},
+{
+  ciudad: "Taltal",
+  temperatura: 26,
+  estado: "Soleado",
+  semanal: [
+    { dia: "Lun", temp: 25, estado: "☀️" },
+    { dia: "Mar", temp: 24, estado: "☀️" },
+    { dia: "Mié", temp: 22, estado: "⛅" },
+    { dia: "Jue", temp: 21, estado: "⛅" },
+    { dia: "Vie", temp: 23, estado: "☀️" }
+  ]
+},
+{
+  ciudad: "San Pedro de Atacama",
+  temperatura: 26,
+  estado: "Soleado",
+  semanal: [
+    { dia: "Lun", temp: 25, estado: "☀️" },
+    { dia: "Mar", temp: 24, estado: "☀️" },
+    { dia: "Mié", temp: 22, estado: "⛅" },
+    { dia: "Jue", temp: 21, estado: "⛅" },
+    { dia: "Vie", temp: 23, estado: "☀️" }
+  ]
+},
+{
+  ciudad: "María Elena",
+  temperatura: 26,
+  estado: "Soleado",
+  semanal: [
+    { dia: "Lun", temp: 25, estado: "☀️" },
+    { dia: "Mar", temp: 24, estado: "☀️" },
+    { dia: "Mié", temp: 22, estado: "⛅" },
+    { dia: "Jue", temp: 21, estado: "⛅" },
+    { dia: "Vie", temp: 23, estado: "☀️" }
+  ]
+},
+{
+  ciudad: "Sierra Gorda",
+  temperatura: 26,
+  estado: "Soleado",
+  semanal: [
+    { dia: "Lun", temp: 25, estado: "☀️" },
+    { dia: "Mar", temp: 24, estado: "☀️" },
+    { dia: "Mié", temp: 22, estado: "⛅" },
+    { dia: "Jue", temp: 21, estado: "⛅" },
+    { dia: "Vie", temp: 23, estado: "☀️" }
+  ]
+},
+{
+  ciudad: "Ollagüe",
+  temperatura: 26,
+  estado: "Soleado",
+  semanal: [
+    { dia: "Lun", temp: 25, estado: "☀️" },
+    { dia: "Mar", temp: 24, estado: "☀️" },
+    { dia: "Mié", temp: 22, estado: "⛅" },
+    { dia: "Jue", temp: 21, estado: "⛅" },
+    { dia: "Vie", temp: 23, estado: "☀️" }
+  ]
+},
+{
+  ciudad: "Balneario Hornitos",
+  temperatura: 19,
+  estado: "Nublado",
+  semanal: [
+    { dia: "Lun", temp: 25, estado: "☀️" },
+    { dia: "Mar", temp: 24, estado: "☀️" },
+    { dia: "Mié", temp: 22, estado: "⛅" },
+    { dia: "Jue", temp: 21, estado: "⛅" },
+    { dia: "Vie", temp: 23, estado: "☀️" }
+  ]
+},
+{
+  ciudad: "Juan López",
+  temperatura: 27,
+  estado: "Soleado",
+  semanal: [
+    { dia: "Lun", temp: 25, estado: "☀️" },
+    { dia: "Mar", temp: 24, estado: "☀️" },
+    { dia: "Mié", temp: 22, estado: "⛅" },
+    { dia: "Jue", temp: 21, estado: "⛅" },
+    { dia: "Vie", temp: 23, estado: "☀️" }
+  ]
+},
+];
+
